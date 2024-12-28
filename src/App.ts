e121b3d32e02651515664beec0621c99e9ee722b
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2024-12-28 21:02:30 +0100

    new change

--- src/App.ts
+++ src/App.ts
@@ -127,7 +127,7 @@ app.use(
   swaggerUi.serve,
   swaggerUi.setup(swaggerDocs, {
     customCssUrl:
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.19.1/swagger-ui.min.css',
   })
 );
 
