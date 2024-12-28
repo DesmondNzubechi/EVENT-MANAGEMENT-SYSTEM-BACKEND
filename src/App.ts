7b9ca726390e7cea546294fa2e50c51aeab6e07a
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2024-12-28 06:02:38 +0100

    New change

--- src/App.ts
+++ src/App.ts
@@ -36,7 +36,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 app.use(cookieParser());
-
+ 
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
